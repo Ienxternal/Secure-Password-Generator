42e58ce5c47442456205ab1a64474e9bc61149ec
Ienxternal <alexanderttnguyen@gmail.com>
2023-03-23 18:02:50 -0500

    Refactoring code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
+// Declaring variables and constants
 const resultEl = document.getElementById('password');
-// const lengthEl = document.getElementById('length');
 const uppercaseEl = document.getElementById('uppercase');
 const lowercaseEl = document.getElementById('lowercase');
 const numbersEl = document.getElementById('numbers');
@@ -8,38 +8,6 @@ const symbolsEl = document.getElementById('symbols');
 const generateEl = document.getElementById('generate');
 const passLength = document.querySelector('#length');
 const passLengthResult = document.querySelector('#length-result')
-const modal = document.getElementById("myModal");
-// Get the button that opens the modal
-const btn = document.getElementById("configure");
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-// Range Selector
-document.addEventListener('DOMContentLoaded', () => {
-  passLength.value = 10  
-  passLengthResult.innerText = 10
-  let onLoadLength = passLength.value
-  result.value = generatePassword(onLoadLength)
-})
-passLength.addEventListener('change', (event) => {
-  passLengthResult.innerText  = event.target.value                   
-})
-
-
 const randomFunc = {
 	lower: getRandomLower,
 	upper: getRandomUpper,
@@ -47,7 +15,32 @@ const randomFunc = {
 	symbol: getRandomSymbol
 }
 
+// Creating modal function upon clicking generate button
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("configure");
+const span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
+//Creating event listener for range selection
+document.addEventListener('DOMContentLoaded', () => {
+  passLength.value = 10  
+  passLengthResult.innerText = 10
+})
+passLength.addEventListener('input', (event) => {
+  passLengthResult.innerText  = event.target.value                   
+})
+
+//Function to declare letters, numbers and symbols on the ASCII table
 function getRandomLower() {
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -61,40 +54,33 @@ function getRandomSymbol() {
 	const symbols = '!@#$%^&*(){}[]=<>/,.'
 	return symbols[Math.floor(Math.random() * symbols.length)];
 }
+
+//Event listener for generate button
 generate.addEventListener('click', () => {
 	const length = +passLength.value;
 	const hasLower = lowercaseEl.checked;
 	const hasUpper = uppercaseEl.checked;
 	const hasNumber = numbersEl.checked;
 	const hasSymbol = symbolsEl.checked;
-	generatedPassword = true;
-
 	resultEl.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
-  generateEl.onclick = function() {
-    modal.style.display = "none";
-  }
+  modal.style.display = "none";
 });
 
+//Function to generate password string using user input and return password string
 function generatePassword(lower, upper, number, symbol, length) {
 	let generatedPassword = '';
 	const typesCount = lower + upper + number + symbol;
 	const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
-	
-	// Doesn't have a selected type
 	if(typesCount === 0) {
 		return '';
 	}
-	
-	// create a loop
 	for(let i=0; i<length; i+=typesCount) {
 		typesArr.forEach(type => {
 			const funcName = Object.keys(type)[0];
 			generatedPassword += randomFunc[funcName]();
 		});
 	}
-	return generatedPassword.slice(0, length)
-									.split('').sort(() => Math.random() - 0.5)
-									.join('');
+	return generatedPassword.slice(0, length).split('').sort(() => Math.random() - 0.5).join('');
 }
 
 
